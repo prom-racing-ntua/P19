ac454fbeac35e81c335a984bbe1bdfdfdebe7b0a
dimoskan <dimoskanavos@gmail.com>
2019-02-14 23:53:05 +0200

    Constant,changing and imported widgets

--- Dashboard_Display/display.py
+++ Dashboard_Display/display.py
@@ -14,15 +14,17 @@ from kivy.uix.button import Button
 from kivy.core.window import Window
 import itertools
 from kivy.uix.widget import Widget
+from screen1 import *
 if __name__ == '__main__':
 	i = 0
 	class Telemetry_App(App):
-		sm = ScreenManager()
+		changing = ScreenManager()
 		def build(self):
+			box= FloatLayout()
 			Window.clearcolor = (85/256,23/256,23/256,1)
 			for i in range(4):
 				screen = Screen(name= 'Title %d' % i)
-				self.sm.add_widget(screen)
+				self.changing.add_widget(screen)
 
 				main=FloatLayout()
 				screen.add_widget(main)
@@ -37,17 +39,27 @@ if __name__ == '__main__':
 				)
 				main.add_widget(
 					Image(
-            		source= "Images/prom.png",
-            		pos_hint= {'x':0.1,'y':0.7},
-            		size_hint= (0.8,0.3)
-            		)
-            	)
+					source= "Images/prom.png",
+					pos_hint= {'x':0.1,'y':0.5},
+					size_hint= (0.8,0.2)
+					)
+				)
+			box.add_widget(self.changing)
+			box.add_widget(imported_widget)
+			box.add_widget(
+					Image(
+					source= "Images/NTUA.png",
+					pos_hint= {'x':0.1,'y':0.8},
+					size_hint= (0.8,0.2)
+					)
+				)
+
 			Clock.schedule_interval(lambda *t: self.letsgo(), 1)
-			return self.sm
+			return box
 		def letsgo(self):
 			global i
 			i+=1
-			self.sm.current = 'Title %d'% (i%4)
+			self.changing.current = 'Title %d'% (i%4)
 	try:
 		Telemetry_App().run()
 	except Exception as e:
---
+++ Dashboard_Display/screen1.py
@@ -0,0 +1,8 @@
+from kivy.uix.label import Label
+
+imported_widget = Label(
+						text= 'I am an imported widget',
+						font_size= '40sp',
+						pos_hint= {'x':0.4,'y':0.1},
+            			size_hint= (0.2,0.2)
+					)
