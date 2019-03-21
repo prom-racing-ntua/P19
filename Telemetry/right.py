8746e4a6046c51d694eff8da47a648efb56f2afe
Grigoris Karaoglanian <grigoris.kara.7@gmail.com>
2019-03-21 19:59:43 +0200

    Auto stash before merge of "master" and "origin/master"
    .

--- Telemetry/right.py
+++ Telemetry/right.py
@@ -13,23 +13,24 @@ right_column = FloatLayout()
 sector1 = Sectors(
 	sectorname = "Sector 1",
 	customcolor = [0.35,0.35,0.35,1],
-	pos_hint = {'x':0.7,'y':0.75},
-	size_hint=(0.1,0.2)
+	pos_hint = {'x':0.7,'y':0.85},
+	size_hint = (0.1,0.15)
+
 )
 right_column.add_widget(sector1)
 
 sector2 = Sectors (
     sectorname = "Sector 2",
     customcolor = [0.55,0.35,0.35,1],
-    pos_hint = {'x':0.8,'y':0.75},
-    size_hint=(0.1,0.2)
+    pos_hint = {'x':0.8,'y':0.85},
+    size_hint=(0.1,0.15)
 )
 right_column.add_widget(sector2)
 
 sector3 = Sectors(
     sectorname = "Sector 3",
     customcolor = [0.35 , 0.55 , 0.35 , 1],
-    pos_hint = {'x':0.9,'y':0.75},
-    size_hint=(0.1,0.2)
+    pos_hint = {'x':0.9,'y':0.85},
+    size_hint=(0.1,0.15)
 )
 right_column.add_widget(sector3)
