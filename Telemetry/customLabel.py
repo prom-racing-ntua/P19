2be9c21014c287fb26d0b75f61bef35d3b4f5e35
Orestis <batsisorestis@gmail.com>
2019-04-04 21:56:45 +0300

    Revert "Auto stash before merge of "master" and "origin/master""
    This reverts commit fa5971ab2209db0b612d76ab17dc88efc17b63fb.

--- Telemetry/customLabel.py
+++ Telemetry/customLabel.py
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import kivy
@@ -35,41 +34,3 @@ class CustomLabel(Label,Widget):
 		self.bgclr=bgclr
 		super(CustomLabel, self).__init__(**kwargs)
 
-=======
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-import kivy
-kivy.require('1.10.0')
-from kivy.properties import StringProperty
-from kivy.uix.widget import Widget
-from kivy.uix.label import Label
-from kivy.graphics import Rectangle,Color
-from kivy.lang import Builder
-from kivy.uix.image import Image
-from hover import HoverBehavior
-
-from kivy.properties import BooleanProperty, ObjectProperty, NumericProperty,ListProperty
-
-
-Builder.load_string('''
-<CustomLabel>:
-    canvas:
-        Color:
-            rgba: [1,1,1,0.1]
-        Rectangle:
-            pos: self.x+self.font_size if self.font_size<=13 else self.x, self.y+self.size[1]/3
-            size: self.font_size*6,self.size[1]/3
-        Color:
-            rgba: self.bgclr
-        Line:
-            rectangle: self.x+self.font_size if self.font_size<=13 else self.x,self.y+self.height/3,self.font_size*6,self.height/3
-            width: 1
-''')
-
-class CustomLabel(Label,Widget):
-	bgclr = ListProperty([1,1,1,0.1])
-	def __init__(self, bgclr, **kwargs):
-		self.bgclr=bgclr
-		super(CustomLabel, self).__init__(**kwargs)
-
->>>>>>> Stashed changes
