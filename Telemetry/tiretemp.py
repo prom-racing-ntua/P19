26eec1cb66d1e4c8dfe6e36bd597bd31c552f7a1
Grigoris Karaoglanian <grigoris.kara.7@gmail.com>
2019-04-12 12:56:16 +0300

    .

--- Telemetry/tiretemp.py
+++ Telemetry/tiretemp.py
@@ -10,3 +10,34 @@ from kivy.properties import NumericProperty,StringProperty, ListProperty
 from kivy.graphics import Rectangle,Color
 from kivy.lang import Builder
 from sectorLabel import SectorLabel
+
+# Builder.load_string('''
+# <TiretempLbl>:
+#     canvas:
+#         Color:
+#             rgba: self.customcolor
+#
+# ''')
+
+class TiretempLbl (Widget) :
+    lblname = StringProperty ()
+    tempclr = ListProperty ([])
+    tiretemp = NumericProperty()
+    mintemp = NumericProperty ()
+    maxtemp = NumericProperty ()
+
+    def __init__(self, **kwargs):
+        super (TiretempLbl,self).__init__ (**kwargs)
+        self.tirelabel = SectorLabel (text = str(tiretemp) , lineclr = [1,1,1,1], bgclr = tempclr , font_size = '18sp'  )
+        self.add_widget (self.tirelabel)
+        ##create the bindings to update size,pos and values
+        self.bind(pos=self._update)
+        self.bind(size=self._update)
+        self.bind(tiretemp=self._change)
+
+    def _update(self,*args):
+        self.tirelabel.center = (self.center_x,self.center_y)
+        self.tirelabel.size = self.size[0],self.size[1]
+
+    def _change (self,*args):
+        self.tirelabel.bgclr = tempclr
