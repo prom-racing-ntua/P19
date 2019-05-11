3247434e7b917a58e3ea4f21a7ebe22096278726
dimoskan <dimoskanavos@gmail.com>
2019-05-11 19:18:11 +0300

    .

--- Shifter/motor_pid_working/motor_pid_working.ino
+++ Shifter/motor_pid_working/motor_pid_working.ino
@@ -5,7 +5,7 @@
 #define M1              5                       // PWM outputs to L298N H-Bridge motor driver module
 #define M2              6
 
-double kp = 27 , ki = 2.5 , kd = 0.01;             // modify for optimal performance
+double kp = 25 , ki = 2 , kd = 0.01;             // modify for optimal performance
 double input = 0, output = 0, setpoint = 0;
 long temp;
 volatile long encoderPos = 0;
@@ -20,23 +20,27 @@ void setup() {
  myPID.SetMode(AUTOMATIC);
  myPID.SetSampleTime(1);
  myPID.SetOutputLimits(-255, 255);
- Serial.begin (115200);                              // for debugging
- setpoint = -30;                       // modify to fit motor and encoder characteristics, potmeter connected to A0
+ Serial.begin (115200);       
+// setpoint=-30;// for debugging
+                       // modify to fit motor and encoder characteristics, potmeter connected to A0
 }
 
 void loop() {
  
+ setpoint=(analogRead(0)-512)*4;
+ 
  input = encoderPos ;                                // data from encoder
- Serial.print(encoderPos);                      // monitor motor position
+                      // monitor motor position
  myPID.Compute();                                    // calculate new output
  pwmOut(output);
-   Serial.print("  -  ");
-  Serial.print(output);// drive L298N H-Bridge module
- Serial.print("  -  ");
-  Serial.println(setpoint);// drive L298N H-Bridge module    
- if((output<=1.5 )&& (output>=-1.7)) {
-  setpoint=0;
- }
+//  Serial.print(analogRead(0));// drive L298N H-Bridge module
+//   Serial.print("  -  ");
+//  Serial.print(output);// drive L298N H-Bridge module
+//   Serial.print("  -  ");
+//  Serial.print(encoderPos);
+// Serial.print("  -  ");
+//  Serial.println(setpoint);// drive L298N H-Bridge module    
+
 }
 
 void pwmOut(int out) {                                // to H-Bridge board
@@ -51,5 +55,5 @@ void pwmOut(int out) {                                // to H-Bridge board
 }
 
 void count1() {
- if (PINE & 0b00010000){encoderPos--;}else{encoderPos++;}
+ if (PIND & 0b00000100){encoderPos--;}else{encoderPos++;}
 }
